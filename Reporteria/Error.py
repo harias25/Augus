d4920a388e3acd7c884160fa3fbc5240fcb070e0
bi <bi@bancaremota.bi.com.gt>
2020-06-04 10:03:35 -0600

    Generación Reporte de Errores

--- Reporteria/Error.py
+++ Reporteria/Error.py
@@ -1,10 +1,10 @@
+from datetime import datetime
+
 class Error():
     def __init__(self,tipoError,descripcion,linea,columna):
         self.tipo = tipoError
         self.error = descripcion
         self.linea = str(linea)
         self.columna = str(columna)
-
-
-    
-
+        self.fechaHora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        
--- Reporteria/ReporteErrores.py
+++ Reporteria/ReporteErrores.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 def func(error):
  
     #Declaración e inicilizacion de la variable "estática"
@@ -13,10 +15,38 @@ def func(error):
 class ReporteErrores():
     def generarReporte(self):
         listado = func(None)
+
+
+        contenido = "<html>" + '\n' + "<head>" + '\n' + "<title>Reporte de Errores RMB</title>" + '\n' + "</head>" + '\n'
+        contenido = contenido + "<body bgcolor=\"black\">" + '\n' + "<center><Font size=22 color=darkred>" + "Reporte de Errores del Archivo del Proyecto" + "</Font></center>" + '\n'
+        contenido = contenido + "<hr >" + '\n' + "<font color=white>" + '\n' + "<center>" + '\n'
+        contenido = contenido + "<table border=1 align=center style=\"width:100%;\" >" + '\n'
+        contenido = contenido + "<TR bgcolor=darkred>" + "\n"
+        contenido = contenido + "<TH  style=\"font-size: 18px; width:20%; color:white\" align=center>Tipo de Error</TH>" + '\n'
+        contenido = contenido + "<TH  style=\"font-size: 18px; width:30%; color:white\" align=center>Descripción del Error</TH>" + '\n'
+        contenido = contenido + "<TH  style=\"font-size: 18px; width:10%; color:white\" align=center>Linea</TH>" + '\n'
+        contenido = contenido + "<TH  style=\"font-size: 18px; width:10%; color:white\" align=center>Columna</TH>" + '\n'
+        contenido = contenido + "<TH  style=\"font-size: 18px; width:10%; color:white\" align=center>Fecha</TH>" + '\n'
+        contenido = contenido + "</TR>" + '\n'
+
+
+
         if(listado == None): listado = []
 
         for error in listado:
-                print(error.error)
-                print("Tipo: "+error.tipo)
-                print("Linea: "+error.linea)
-                print("Columna: "+error.columna)
+            contenido = contenido + "<TD style=\"font-size: 15px; color:white;\" align=center>" + error.tipo + "</TD>" + '\n'
+            contenido = contenido + "<TD style=\"font-size: 15px; color:white;\" color:white align=center>" + error.error + "</TD>" + '\n'
+            contenido = contenido + "<TD style=\"font-size: 15px; color:white;\" color:white align=center>" + error.linea + "</TD>" + '\n'
+            contenido = contenido + "<TD style=\"font-size: 15px; color:white;\" color:white align=center>" + error.columna + "</TD>" + '\n'
+            contenido = contenido + "<TD style=\"font-size: 15px; color:white;\" color:white align=center>" + error.fechaHora + "</TD>" + '\n'
+            contenido = contenido + "</TR>" + '\n'
+
+
+        contenido = contenido + '\n' + "</center>" + '\n' + "</table>" + "</body>" + '\n' + "</html>"
+
+        f = open ('./reporteErrores.html','w')
+        f.write(contenido)
+        f.close()
+
+        webbrowser.open_new_tab('./reporteErrores.html')
+        
